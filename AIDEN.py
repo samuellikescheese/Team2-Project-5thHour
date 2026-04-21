24ca80329a886c73af5c43f19322c49c53f13ad4
Aiden Funkhouser <af128219@student.wpsok.org>
2026-04-21 13:11:50 -0500

    Aiden

---
+++ AIDEN.py
@@ -0,0 +1,7 @@
+# Hi I'm Aiden and I will be making the armor.
+def Aiden():
+    class armor:
+        def __init__(self,name,AC,health):
+            self.name=name
+            self.AC=AC
+            self.health=health
