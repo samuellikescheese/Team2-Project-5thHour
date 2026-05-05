d5d76b0671452d52f2475d76d4838c1fcdeffa1d
Samuel Raymond <sr128226@student.wpsok.org>
2026-05-05 12:38:02 -0500

    Master File

--- Master.py
+++ Master.py
@@ -1,7 +1,6 @@
 #Scrum document: https://docs.google.com/document/d/1_zBj3TbMbulobdO4XD9UXrmrTHvDDRmD4JMZqRfPbXE/edit?tab=t.0
 
 import random, time
-time.sleep(0.000000001)
 print ("welcome to Gladiator Battle Simulator")
 print ("Your first gladiator")
 changer = 1
@@ -25,23 +24,24 @@ Charge = armor('Charge',13)
 Tank = armor('Tank',8)
 lister = ["Leather","Iron","Samurai","Overgrown","Champion","Radiant","Ninja","Cursed","Charge","Tank"]
 class weapon:
-    def __init__(self,sub,DG):
+    def __init__(self,sub,DG,Atkmod):
         self.sub=sub
         self.DG=DG
-Gladius = weapon('Gladius',random.randint(1,12))
-Spatha = weapon('Spatha',random.randint(2,6))
-Pugio = weapon('Pugio',random.randint(2,4))
-Hasta = weapon('Hasta',random.randint(2,8))
-Fascina = weapon('Fascina',random.randint(2,6))
-Semispatha = weapon('Semispatha',random.randint(3,4))
-Lancea = weapon('Lancea',10)
-Sica = weapon('Sica',random.randint(3,6))
-Pilum = weapon('Pilum',random.randint(1,12))
-Arcus = weapon('Arcus',random.randint(3,4))
+        self.Atkmod=Atkmod
+Gladius = weapon('Gladius',random.randint(1,12),3)
+Spatha = weapon('Spatha',random.randint(2,6),2)
+Pugio = weapon('Pugio',random.randint(2,4),4)
+Hasta = weapon('Hasta',random.randint(2,8),1)
+Fascina = weapon('Fascina',random.randint(2,6),3)
+Semispatha = weapon('Semispatha',random.randint(3,4),3)
+Lancea = weapon('Lancea',10,5)
+Sica = weapon('Sica',random.randint(3,6),1)
+Pilum = weapon('Pilum',random.randint(1,12),2)
+Arcus = weapon('Arcus',random.randint(1,4),0)
 weaplist = ["Gladius","Spatha","Pugio","Hasta","Fascina","Semispatha","Lancea","Sica","Pilum","Arcus"]
 def master():
     class Gladi:
-        def __init__(self,title,health,damage,fAC,back,WN,AN):
+        def __init__(self,title,health,damage,fAC,back,WN,AN,cheese):
             self.title=title
             self.health=health
             self.damage=damage
@@ -49,8 +49,9 @@ def master():
             self.back=back
             self.WN=WN
             self.AN=AN
-    fighter1 = Gladi(0,100,0,0,0,"","")
-    fighter2 = Gladi(0,100,0,0,0,"","")
+            self.cheese=cheese
+    fighter1 = Gladi(0,100,0,0,0,"","",0)
+    fighter2 = Gladi(0,100,0,0,0,"","",0)
     def namer():
         Name = input("Type your gladiators Name ")
         if Name == "":
@@ -78,48 +79,68 @@ def master():
                     wa = weaplist[Weapon - 1]
                     if Weapon == 1:
                         fighter1.damage = Gladius.DG
+                        fighter1.cheese = Gladius.Atkmod
                     if Weapon == 2:
                         fighter1.damage = Spatha.DG
+                        fighter1.cheese = Spatha.Atkmod
                     if Weapon == 3:
                         fighter1.damage = Pugio.DG
+                        fighter1.cheese = Pugio.Atkmod
                     if Weapon == 4:
                         fighter1.damage = Hasta.DG
+                        fighter1.cheese = Hasta.Atkmod
                     if Weapon == 5:
                         fighter1.damage = Fascina.DG
+                        fighter1.cheese = Fascina.Atkmod
                     if Weapon == 6:
                         fighter1.damage = Semispatha.DG
+                        fighter1.cheese = Semispatha.Atkmod
                     if Weapon == 7:
                         fighter1.damage = Lancea.DG
+                        fighter1.cheese = Lancea.Atkmod
                     if Weapon == 8:
                         fighter1.damage = Sica.DG
+                        fighter1.cheese = Sica.Atkmod
                     if Weapon == 9:
                         fighter1.damage = Pilum.DG
+                        fighter1.cheese = Pilum.Atkmod
                     if Weapon == 10:
                         fighter1.damage = Arcus.DG
+                        fighter1.cheese = Arcus.Atkmod
                 else:
                     print(f"Your 2nd gladiator, {fighter1.title}s weapon is {weaplist[Weapon - 1]}")
                     global wb
                     wb = weaplist[Weapon - 1]
                     if Weapon == 1:
                         fighter2.damage = Gladius.DG
+                        fighter2.cheese = Gladius.Atkmod
                     if Weapon == 2:
                         fighter2.damage = Spatha.DG
+                        fighter2.cheese = Spatha.Atkmod
                     if Weapon == 3:
                         fighter2.damage = Pugio.DG
+                        fighter2.cheese = Pugio.Atkmod
                     if Weapon == 4:
                         fighter2.damage = Hasta.DG
+                        fighter2.cheese = Hasta.Atkmod
                     if Weapon == 5:
                         fighter2.damage = Fascina.DG
+                        fighter2.cheese = Fascina.Atkmod
                     if Weapon == 6:
                         fighter2.damage = Semispatha.DG
+                        fighter2.cheese = Semispatha.Atkmod
                     if Weapon == 7:
                         fighter2.damage = Lancea.DG
+                        fighter2.cheese = Lancea.Atkmod
                     if Weapon == 8:
                         fighter2.damage = Sica.DG
+                        fighter2.cheese = Sica.Atkmod
                     if Weapon == 9:
                         fighter2.damage = Pilum.DG
+                        fighter2.cheese = Pilum.Atkmod
                     if Weapon == 10:
                         fighter2.damage = Arcus.DG
+                        fighter2.cheese = Arcus.Atkmod
         except ValueError:
             print ("That is not a number")
             forge()
@@ -215,5 +236,69 @@ def master():
         print("Welcome to the COLOSSEUM")
         print(fighter1.title, wa, aa, fighter1.back)
         print(fighter2.title, wb, ab, fighter2.back)
+        roll1 = random.randint(1, 20)
+        roll2 = random.randint(1, 20)
+        print(f"{fighter1} VS {fighter2}")
+        time.sleep(0.5)
+        if roll1 >= roll2:
+            while 5 > 3:
+                print("Gale attacks the Mindflayer")
+                time.sleep(0.5)
+                hero = random.randint(1, 20)
+                evil = random.randint(1, 20)
+                if not hero == 1 or hero + fighter1.cheese >= fighter2.fAC or hero == 20:
+                    fighter2.health -= fighter1.damage
+                    if fighter2.health <= 0:
+                        print("the Mindflayer is dead")
+                        exit()
+                    else:
+                        print("the Mindflayer stands")
+                        time.sleep(0.5)
+                else:
+                    print("he misses")
+                    time.sleep(0.5)
+                print("the Mindflayer attacks")
+                time.sleep(0.5)
+                if not evil == 1 or evil + fighter2.cheese >= fighter1.fAC or evil == 20:
+                    fighter1.health -= fighter2.cheese
+                    if fighter1.health <= 0:
+                        print("Gale is dead")
+                        exit()
+                    else:
+                        print("Gale stands")
+                        time.sleep(0.5)
+                else:
+                    print("he misses")
+                    time.sleep(0.5)
+        if roll2 > roll1:
+            while 5 > 3:
+                print("the Mindflayer attacks")
+                time.sleep(0.5)
+                hero = random.randint(1, 20)
+                evil = random.randint(1, 20)
+                if not evil == 1 or evil + fighter2.cheese >= fighter1.fAC or evil == 20:
+                    fighter1.health -= fighter2.cheese
+                    if fighter1.health <= 0:
+                        print("Gale is dead")
+                        exit()
+                    else:
+                        print("Gale stands")
+                        time.sleep(0.5)
+                else:
+                    print("he misses")
+                    time.sleep(0.5)
+                print("gale attacks the Mindflayer")
+                time.sleep(0.5)
+                if not hero == 1 or hero + fighter1.cheese >= fighter2.fAC or hero == 20:
+                    fighter2.health -= fighter1.damage
+                    if fighter2.health <= 0:
+                        print("the Mindflayer is dead")
+                        exit()
+                    else:
+                        print("the Mindflayer stands")
+                        time.sleep(0.5)
+                else:
+                    print("he misses")
+                    time.sleep(0.5)
     battle()
 master()
